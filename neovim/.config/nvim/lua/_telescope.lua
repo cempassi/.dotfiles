c95a5b3fd879b64d88a05119c249b4d03fe08e6e
Cedric M'Passi <cempassi@student.42.fr>
2021-09-27 15:17:00 +0200

    Move telescope mappings, no addition

--- neovim/.config/nvim/lua/_telescope.lua
+++ neovim/.config/nvim/lua/_telescope.lua
@@ -35,40 +35,38 @@ require('telescope').setup {
     },
     git_icons = {added = "☺", changed = "ƣ", deleted = "ㄐ"},
     mappings = {
-	i = {
-      ["<C-n>"] = actions.move_selection_next,
-      ["<C-p>"] = actions.move_selection_previous,
+      i = {
+        ["<C-n>"] = actions.move_selection_next,
+        ["<C-p>"] = actions.move_selection_previous,
 
-      ["<leader>q"] = actions.close,
+        ["<leader>q"] = actions.close,
 
-      ["<C-j>"] = actions.move_selection_next,
-      ["<C-k>"] = actions.move_selection_previous,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
 
-      ["<CR>"] = actions.select_default + actions.center,
-      ["<leader>hs"] = actions.select_horizontal,
-      ["<leader>vs"] = actions.select_vertical,
-      ["<leader>te"] = actions.select_tab,
+        ["<CR>"] = actions.select_default + actions.center,
+        ["<leader>hs"] = actions.select_horizontal,
+        ["<leader>vs"] = actions.select_vertical,
+        ["<leader>te"] = actions.select_tab,
 
-      ["<C-u>"] = actions.preview_scrolling_up,
-      ["<C-d>"] = actions.preview_scrolling_down,
+        ["<C-u>"] = actions.preview_scrolling_up,
+        ["<C-d>"] = actions.preview_scrolling_down,
 
-    },
+      },
 
-    n = {
-      ["<leader>q"] = actions.close,
-      ["<CR>"] = actions.select_default + actions.center,
-      ["<C-x>"] = actions.select_horizontal,
-      ["<C-v>"] = actions.select_vertical,
-      ["<C-t>"] = actions.select_tab,
+      n = {
+        ["<leader>q"] = actions.close,
+        ["<CR>"] = actions.select_default + actions.center,
+        ["<C-x>"] = actions.select_horizontal,
+        ["<C-v>"] = actions.select_vertical,
+        ["<C-t>"] = actions.select_tab,
 
-      ["j"] = actions.move_selection_next,
-      ["k"] = actions.move_selection_previous,
+        ["j"] = actions.move_selection_next,
+        ["k"] = actions.move_selection_previous,
 
-      ["<C-u>"] = actions.preview_scrolling_up,
-      ["<C-d>"] = actions.preview_scrolling_down,
+        ["<C-u>"] = actions.preview_scrolling_up,
+        ["<C-d>"] = actions.preview_scrolling_down,
       }
     }
   }
 }
-
---require('telescope').load_extension('ghcli')
