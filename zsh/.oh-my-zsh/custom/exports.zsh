4f323a3debfbda9833e299ec652d0a9e9a78f2f5
Cedric M'Passi <cempassi@student.42.fr>
2021-10-03 22:39:13 +0200

    Use vim as EDITOR variable

--- zsh/.oh-my-zsh/custom/exports.zsh
+++ zsh/.oh-my-zsh/custom/exports.zsh
@@ -6,6 +6,8 @@ export PYENV_ROOT=$HOME/.pyenv
 
 export PATH=$PYENV_ROOT/bin:$PATH:./node_modules/.bin
 
+export EDITOR=vim
+
 export MANPAGER="col -b | nvim -RO  '+set ft=man ts=8 nomod nolist nonu' '+nnoremap i <nop>' -"
 
 export LSCOLORS="exfxcxdxbxegedabagacad"
