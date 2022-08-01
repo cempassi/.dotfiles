40ecaccaeb4de6001e714d451dff289ce562923e
Cedric M'Passi <cempassi@student.42.fr>
2022-08-02 01:31:08 +0200

    Add home-manager config

--- home-manager/.config/nixpkgs/home.nix
+++ home-manager/.config/nixpkgs/home.nix
@@ -1,21 +1,68 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "cempassi";
-  home.homeDirectory = "/Users/cedric.mpassi";
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "22.05";
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  home = {
+    username = "cempassi";
+    homeDirectory = "/Users/cedric.mpassi";
+
+    sessionVariables = {
+      EDITOR = "nvim";
+      PYENV_ROOT = "${config.home.homeDirectory}/.pyenv";
+      LSCOLORS = "exfxcxdxbxegedabagacad";
+      SSH_KEY_PATH = "~/.ssh/rsa_id";
+      BAT_THEME = "Nord";
+      KEYTIMEOUT = 1;
+      PAGER = "bat --paging=auto --plain";
+      MAIL = "cempassi@student.42.fr";
+    };
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "22.05";
+  };
+
+  home.packages = [
+    # Text Editor
+    pkgs.neovim
+    pkgs.terraform-ls
+    pkgs.stylua
+    pkgs.black
+    pkgs.selene
+    pkgs.shfmt
+
+    # Rust Alternatives
+    pkgs.ripgrep
+    pkgs.tokei
+    pkgs.delta
+    pkgs.bat
+    pkgs.exa
+    pkgs.zoxide
+    pkgs.fd
+    pkgs.bottom
+    pkgs.diskonaut
+    pkgs.du-dust
+
+    # Code managers
+    pkgs.gh
+    pkgs.glab
+
+    # Container
+    pkgs.docker
+
+    # Utils
+    pkgs.stow
+    pkgs.bitwarden-cli
+    pkgs.tmux
+    pkgs.jq
+    pkgs.fzf
+  ];
 }
