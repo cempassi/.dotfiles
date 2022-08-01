bb99e0a9fffbe666da23725fadb8373e413ee8de
Cedric M'Passi <cempassi@student.42.fr>
2022-08-02 01:34:59 +0200

    Move env var to home manager configurationx

--- zsh/.oh-my-zsh/custom/exports.zsh
+++ zsh/.oh-my-zsh/custom/exports.zsh
@@ -6,20 +6,4 @@ export PYENV_ROOT=$HOME/.pyenv
 
 export PATH=$PYENV_ROOT/bin:$PATH:./node_modules/.bin
 
-export EDITOR=vim
-
-export MANPAGER="col -b | nvim -RO  '+set ft=man ts=8 nomod nolist nonu' '+nnoremap i <nop>' -"
-
-export LSCOLORS="exfxcxdxbxegedabagacad"
-
-export CDPATH=:..:~:~/Programming:~/Programming/PMU:~/Programming/42:~/Programming/Web:~/Programming/C:~/Programming/nvim
-
-export BAT_THEME=Nord
-
-export SSH_KEY_PATH="~/.ssh/rsa_id"
-
-export KEYTIMEOUT=1
-
-export GPG_TTY=${TTY}
-
-export PAGER="bat --paging=auto --plain"
+export MANPAGER="nvim -RO +Man!"
