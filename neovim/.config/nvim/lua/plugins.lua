b495b67eee0155ffe9cbff67e7b6bf334a0df671
Cedric M'Passi <cempassi@student.42.fr>
2022-08-13 22:05:21 +0200

    Replace GiGutter by Gitsigns and format plugins.lua

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,201 +1,213 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 local config = {
-  git = {
-    default_url_format = 'git@github.com:%s' -- Lua format string used for "aaa/bbb" style plugins
-  },
+	git = {
+		default_url_format = "git@github.com:%s", -- Lua format string used for "aaa/bbb" style plugins
+	},
 }
 
-return require('packer').startup({function()
-  use {'wbthomason/packer.nvim', opt = true}
+return require("packer").startup({
+	function()
+		use({ "wbthomason/packer.nvim", opt = true })
 
-  -- Increase startup time
-  use 'lewis6991/impatient.nvim'
+		-- Increase startup time
+		use("lewis6991/impatient.nvim")
 
-  -- Status bar
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  }
+		-- Status bar
+		use({
+			"nvim-lualine/lualine.nvim",
+			requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		})
 
-  -- Completion
-  use {
-    'hrsh7th/nvim-cmp',
-    requires = {
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'ray-x/cmp-treesitter'},
-      {'f3fora/cmp-spell'}
-    }
-  }
+		-- Completion
+		use({
+			"hrsh7th/nvim-cmp",
+			requires = {
+				{ "hrsh7th/cmp-nvim-lsp" },
+				{ "hrsh7th/cmp-buffer" },
+				{ "hrsh7th/cmp-path" },
+				{ "saadparwaiz1/cmp_luasnip" },
+				{ "ray-x/cmp-treesitter" },
+				{ "f3fora/cmp-spell" },
+			},
+		})
 
-  use {
-    'L3MON4D3/LuaSnip',
-    requires = {
-      {'rafamadriz/friendly-snippets'}
-    }
-  }
+		use({
+			"L3MON4D3/LuaSnip",
+			requires = {
+				{ "rafamadriz/friendly-snippets" },
+			},
+		})
 
-  -- Dashboard
-  use 'glepnir/dashboard-nvim'
-   
-  -- Lua
-  use 'olimorris/persisted.nvim'
+		-- Dashboard
+		use("glepnir/dashboard-nvim")
 
-  -- Nix syntax
-  use 'LnL7/vim-nix'
+		-- Lua
+		use("olimorris/persisted.nvim")
 
-  use 'onsails/lspkind-nvim'
+		-- Nix syntax
+		use("LnL7/vim-nix")
 
-  -- Auto Pairs
-  use 'windwp/nvim-autopairs'
+		use("onsails/lspkind-nvim")
 
-  use {
-    'VonHeikemen/fine-cmdline.nvim',
-    requires = {
-      {'MunifTanjim/nui.nvim'}
-    }
-  }
+		-- Auto Pairs
+		use("windwp/nvim-autopairs")
 
-  -- Treesitter
-  use 'nvim-treesitter/nvim-treesitter'
+		use({
+			"VonHeikemen/fine-cmdline.nvim",
+			requires = {
+				{ "MunifTanjim/nui.nvim" },
+			},
+		})
 
-  -- Code Navigation
-  use 'ggandor/leap.nvim'
-  use 'tpope/vim-repeat'
+		-- Treesitter
+		use("nvim-treesitter/nvim-treesitter")
 
-  -- LSP
-  use 'neovim/nvim-lspconfig'
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use { 'tami5/lspsaga.nvim' } 
+		-- Code Navigation
+		use("ggandor/leap.nvim")
+		use("tpope/vim-repeat")
 
-  -- Notification
-  use 'rcarriga/nvim-notify'
+		-- LSP
+		use("neovim/nvim-lspconfig")
+		use("jose-elias-alvarez/null-ls.nvim")
+		use("tami5/lspsaga.nvim")
 
-  -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {
-      {'nvim-lua/plenary.nvim'},
-      {'nvim-lua/popup.nvim'},
-      {'ANGkeith/telescope-terraform-doc.nvim'}
-    }
-  }
+		-- Notification
+		use("rcarriga/nvim-notify")
 
-  use 'nvim-telescope/telescope-github.nvim'
+		-- Telescope
+		use({
+			"nvim-telescope/telescope.nvim",
+			requires = {
+				{ "nvim-lua/plenary.nvim" },
+				{ "nvim-lua/popup.nvim" },
+				{ "ANGkeith/telescope-terraform-doc.nvim" },
+			},
+		})
 
-  -- Debugger
-  use 'mfussenegger/nvim-dap'
+		use("nvim-telescope/telescope-github.nvim")
 
-  -- File Navigation
-  use "elihunter173/dirbuf.nvim"
+		-- Debugger
+		use("mfussenegger/nvim-dap")
 
-  -- Git integration
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-git'
-  use 'airblade/vim-gitgutter'
-  use 'rhysd/git-messenger.vim'
+		-- File Navigation
+		use({
+			"elihunter173/dirbuf.nvim",
+			commit = "4cd1a2c0b2c2fb9e06070c8152592630737eb55c",
+		})
 
-  -- Floating terminal
-  use 'voldikss/vim-floaterm'
+		-- Git integration
+		use("tpope/vim-fugitive")
+		use("tpope/vim-git")
+		--use("airblade/vim-gitgutter")
+		use("rhysd/git-messenger.vim")
+		use({
+			"lewis6991/gitsigns.nvim",
+			tag = "release", -- To use the latest release
+		})
 
-  -- Async tasks
-  use 'skywind3000/asynctasks.vim'
-  use 'skywind3000/asyncrun.vim'
+		-- Floating terminal
+		use("voldikss/vim-floaterm")
 
-  -- Startup
-  use 'dstein64/vim-startuptime'
+		-- Async tasks
+		use("skywind3000/asynctasks.vim")
+		use("skywind3000/asyncrun.vim")
 
-  -- Theme
-  use 'cempassi/monochrome.nvim'
-  use {
-    "mcchrish/zenbones.nvim",
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    requires = "rktjmp/lush.nvim"
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-  }
+		-- Startup
+		use("dstein64/vim-startuptime")
 
-  use 'themercorp/themer.lua'
+		-- Theme
+		use("cempassi/monochrome.nvim")
+		use({
+			"mcchrish/zenbones.nvim",
+			-- Optionally install Lush. Allows for more configuration or extending the colorscheme
+			requires = "rktjmp/lush.nvim",
+			-- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+		})
 
-  -- Key Mappings
-  use "folke/which-key.nvim"
+		use("themercorp/themer.lua")
 
-  -- Zen mode
-  use "Pocco81/TrueZen.nvim"
+		-- Markdown
+		use("ellisonleao/glow.nvim")
+		use("jghauser/follow-md-links.nvim")
 
-  -- Syntax Higlighting
-  use 'cespare/vim-toml'
+		-- Key Mappings
+		use("folke/which-key.nvim")
 
-  -- Window scrolling
-  use 'karb94/neoscroll.nvim'
+		-- Zen mode
+		use("Pocco81/TrueZen.nvim")
 
-  -- Rust
-  use {
-    'saecki/crates.nvim',
-    tag = 'v0.2.1',
-    requires = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('crates').setup()
-    end,
-  }
+		-- Syntax Higlighting
+		use("cespare/vim-toml")
 
-  -- Rest api
-  use {
-    "NTBBloodbath/rest.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("rest-nvim").setup({
-        -- Open request results in a horizontal split
-        result_split_horizontal = false,
-        -- Keep the http file buffer above|left when split horizontal|vertical
-        result_split_in_place = false,
-        -- Skip SSL verification, useful for unknown certificates
-        skip_ssl_verification = false,
-        -- Highlight request on run
-        highlight = {
-          enabled = true,
-          timeout = 150,
-        },
-        result = {
-          -- toggle showing URL, HTTP info, headers at top the of result window
-          show_url = true,
-          show_http_info = true,
-          show_headers = true,
-        },
-        -- Jump to request line on run
-        jump_to_request = false,
-        env_file = '.env',
-        custom_dynamic_variables = {},
-        yank_dry_run = true,
-      })
-    end
-  }
+		-- Window scrolling
+		use("karb94/neoscroll.nvim")
 
-  -- Poject Management
-  use 'ahmedkhalf/project.nvim'
+		-- Rust
+		use({
+			"saecki/crates.nvim",
+			tag = "v0.2.1",
+			requires = { "nvim-lua/plenary.nvim" },
+			config = function()
+				require("crates").setup()
+			end,
+		})
 
-  -- Misc
-  use 'pbondoer/vim-42header'
-  use 'liuchengxu/vim-which-key'
-  use 'kyazdani42/nvim-web-devicons'
-  use 'norcalli/nvim-colorizer.lua'
-  use 'hashivim/vim-terraform'
-  use 'editorconfig/editorconfig-vim'
+		-- Rest api
+		use({
+			"NTBBloodbath/rest.nvim",
+			requires = { "nvim-lua/plenary.nvim" },
+			config = function()
+				require("rest-nvim").setup({
+					-- Open request results in a horizontal split
+					result_split_horizontal = false,
+					-- Keep the http file buffer above|left when split horizontal|vertical
+					result_split_in_place = false,
+					-- Skip SSL verification, useful for unknown certificates
+					skip_ssl_verification = false,
+					-- Highlight request on run
+					highlight = {
+						enabled = true,
+						timeout = 150,
+					},
+					result = {
+						-- toggle showing URL, HTTP info, headers at top the of result window
+						show_url = true,
+						show_http_info = true,
+						show_headers = true,
+					},
+					-- Jump to request line on run
+					jump_to_request = false,
+					env_file = ".env",
+					custom_dynamic_variables = {},
+					yank_dry_run = true,
+				})
+			end,
+		})
 
-  -- Lua development
-  --use 'bfredl/nvim-luadev'
-  use 'nvim-treesitter/playground'
-  --use {'glacambre/firenvim'}
+		-- Poject Management
+		use("ahmedkhalf/project.nvim")
 
-  --local development, only use at home
-  --use '~/Programming/nvim/nvim-dap'
-  --use '~/Programming/nvim/nvim-dap-virtual-text'
-  --use '~/Programming/nvim/plenary.nvim'
-  --use '~/Programming/nvim/telescope.nvim'
-  --use '~/Programming/nvim/telescope-packer.nvim'
-  --use '~/Programming/nvim/telescope-github.nvim'
-end,
-config = config
+		-- Misc
+		use("pbondoer/vim-42header")
+		use("liuchengxu/vim-which-key")
+		use("kyazdani42/nvim-web-devicons")
+		use("norcalli/nvim-colorizer.lua")
+		use("hashivim/vim-terraform")
+		use("editorconfig/editorconfig-vim")
+
+		-- Lua development
+		--use 'bfredl/nvim-luadev'
+		use("nvim-treesitter/playground")
+		--use {'glacambre/firenvim'}
+
+		--local development, only use at home
+		--use '~/Programming/nvim/nvim-dap'
+		--use '~/Programming/nvim/nvim-dap-virtual-text'
+		--use '~/Programming/nvim/plenary.nvim'
+		--use '~/Programming/nvim/telescope.nvim'
+		--use '~/Programming/nvim/telescope-packer.nvim'
+		--use '~/Programming/nvim/telescope-github.nvim'
+	end,
+	config = config,
 })
