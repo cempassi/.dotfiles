bef185144ac174d5c26544ec0e24fd6109d16bc1
Cedric M'Passi <cempassi@student.42.fr>
2022-08-13 22:28:20 +0200

    Add glow and lspkind configs

---
+++ neovim/.config/nvim/lua/_glow.lua
@@ -0,0 +1,6 @@
+require('glow').setup({
+  style = "dark",
+  width = 250,
+  height = 250,
+  border = "rounded"
+})
---
+++ neovim/.config/nvim/lua/_lspkind.lua
@@ -0,0 +1,39 @@
+require("lspkind").init({
+	-- enables text annotations
+	--
+	-- default: true
+	mode = "symbol_text",
+
+	-- default symbol map
+	-- can be either 'default' (requires nerd-fonts font) or
+	-- 'codicons' for codicon preset (requires vscode-codicons font)
+	--
+	-- default: 'default'
+	preset = "codicons",
+
+	-- override preset symbols
+	--
+	-- default: {}
+	symbol_map = {
+		Text = "",
+		Method = "ƒ",
+		Function = "ﬦ",
+		Constructor = "",
+		Variable = "",
+		Class = "",
+		Interface = "ﰮ",
+		Module = "",
+		Property = "",
+		Unit = "",
+		Value = "",
+		Enum = "了",
+		Keyword = "",
+		Snippet = "﬌",
+		Color = "",
+		File = "",
+		Folder = "",
+		EnumMember = "",
+		Constant = "",
+		Struct = "",
+	},
+})
