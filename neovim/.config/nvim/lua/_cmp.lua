4f9709ee07126393a3af0a2ec01a2c07686b5462
Cedric M'Passi <cempassi@student.42.fr>
2022-08-15 11:01:46 +0200

    Update cmp configuration

--- neovim/.config/nvim/lua/_cmp.lua
+++ neovim/.config/nvim/lua/_cmp.lua
@@ -1,88 +1,93 @@
 local luasnip = require("luasnip")
-local cmp = require'cmp'
-local lspkind = require('lspkind')
+local cmp = require("cmp")
+local lspkind = require("lspkind")
 
 vim.o.completeopt = "menu,menuone,noselect"
 
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body) -- For `luasnip` users.
-    end,
-  },
-  window = {
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = {
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+	snippet = {
+		expand = function(args)
+			local luasnip = prequire("luasnip")
+			if not luasnip then
+				return
+			end
+			luasnip.lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	},
+	window = {
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = {
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    })
-  },
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol_text', 
-      menu = ({
-      nvim_lsp = "[LSP]",
-      treesitter = "[Treesitter]",
-      luasnip = "[Snippet]",
-      path = "[Path]",
-      {
-        name = "buffer",
-        opts = {
-          get_bufnrs = function()
-            local bufs = {}
-            for _, win in ipairs(vim.api.nvim_list_wins()) do
-              bufs[vim.api.nvim_win_get_buf(win)] = true
-            end
-            return vim.tbl_keys(bufs)
-          end
-        },
-      },
-      crates = "[Crate]",
-    })}),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'treesitter'},
-    { name = 'luasnip' },
-    { name = 'spell'},
-    { name = 'path' },
-    { name = 'buffer' },
-    { name = "crates" },
-  }
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+		["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+	},
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol_text",
+			menu = {
+				nvim_lsp = "[LSP]",
+				treesitter = "[Treesitter]",
+				luasnip = "[Snippet]",
+				path = "[Path]",
+				{
+					name = "buffer",
+					opts = {
+						get_bufnrs = function()
+							local bufs = {}
+							for _, win in ipairs(vim.api.nvim_list_wins()) do
+								bufs[vim.api.nvim_win_get_buf(win)] = true
+							end
+							return vim.tbl_keys(bufs)
+						end,
+					},
+				},
+				crates = "[Crate]",
+			},
+		}),
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "treesitter" },
+		{ name = "luasnip" },
+		{ name = "spell" },
+		{ name = "path" },
+		{ name = "buffer" },
+		{ name = "crates" },
+	},
 })
