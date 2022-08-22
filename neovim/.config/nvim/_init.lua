d428bd9a0be68f1010985865bd1bab6930511eb0
Cedric M'Passi <cempassi@student.42.fr>
2022-08-22 23:43:19 +0200

    Start migration to init.lua

---
+++ neovim/.config/nvim/_init.lua
@@ -0,0 +1,54 @@
+--                                %@@@@@@@@@@@@@@@@@/
+--                         %@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,
+--                     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+--                 .@@@@@@@@  ,@@@@@@,           &@@@@@@   @@@@@@@@
+--               @@@@@@@     @@@@@#                 @@@@@@     @@@@@@@
+--             @@@@@@       @@@@@     @@@@@@@   .     @@@@@       @@@@@@
+--           @@@@@@        &@@@@     @@@@@@@&    @     @@@@,        @@@@@@
+--         &@@@@@          @@@@@    %@@@@@@@@@@@@@,    @@@@&          @@@@@*
+--           @@@@@@        &@@@@     @@@@@@@@@@@@@     @@@@         @@@@@@
+--             @@@@@@       @@@@@      @@@@@@@@@      @@@@@      ,@@@@@@
+--               @@@@@@@     @@@@@@                 @@@@@@    *@@@@@@@
+--                  @@@@@@@@   @@@@@@@           @@@@@@@  ,@@@@@@@@
+--                     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(
+--                          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+--                                 @@@@@@@@@@@@@@@@%
+
+local cmd = vim.cmd
+local fn = vim.fn
+
+_G.load_config = function()
+	require("config.options")
+
+	--require("mange.theme").setup()
+	--require("mange.mappings").setup()
+
+	--require("mange.cursorline").setup()
+	--require("mange.yank_highlight").setup()
+	--require("mange.vimdiff").setup()
+
+	--require("mange.autocommands")
+end
+
+--
+-- Startup (and bootstapping)
+--
+-- Bootstrap Packer if not installed already
+do
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.isdirectory(install_path) == 0 then
+		fn.system({
+			"git",
+			"clone",
+			"https://github.com/wbthomason/packer.nvim",
+			install_path,
+		})
+		cmd("packadd packer.nvim")
+		require("plugins")
+		require("packer").sync()
+		cmd("autocmd User PackerComplete ++once lua load_config()")
+	else
+		require("plugins")
+		load_config()
+	end
+end
