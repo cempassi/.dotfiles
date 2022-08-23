ae6358a2c59f135038ca87a604e145e0a3a1b47b
Cedric M'Passi <cempassi@student.42.fr>
2022-08-24 00:49:33 +0200

    Add JSON formatter

--- neovim/.config/nvim/lua/_null-ls.lua
+++ neovim/.config/nvim/lua/_null-ls.lua
@@ -3,7 +3,9 @@ require("null-ls").setup({
 	sources = {
 		builtins.formatting.stylua, -- lua
 		builtins.formatting.alejandra, -- Nix
-    builtins.formatting.prettier, --  JS/TS, JSON,
+    builtins.formatting.prettier.with({
+      filetypes = {"html", "json", "yaml", "markdown", "telekasten", "js", "ts"}
+    }), --  JS/TS, JSON,
 		builtins.formatting.shfmt, --shell
 		builtins.formatting.terraform_fmt, --terraform
 		builtins.formatting.black.with({ extra_args = { "--fast" } }), --python
