7662cad527933d0f7d0eeeabee4309d5efbca3eb
Cedric M'Passi <cempassi@student.42.fr>
2022-10-29 02:02:46 +0200

    nix: move packages in separate file

--- home.nix
+++ home.nix
@@ -7,6 +7,8 @@
   programs.home-manager.enable = true;
 
   home = {
+    imports = [./config/git/git.nix];
+
     username = "cempassi";
     homeDirectory = "/Users/cedric.mpassi";
 
@@ -43,58 +45,8 @@
     stateVersion = "22.05";
   };
 
-  home.file.".config/git/work".source = ./git/.gitconfig-work;
-
-  imports = [ ./config/git/git.nix];
-
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
   };
-
-  home.packages = [
-    # Text Editor
-    pkgs.neovim
-    pkgs.terraform-ls
-    pkgs.stylua
-    pkgs.black
-    pkgs.selene
-    pkgs.shfmt
-    pkgs.alejandra
-    pkgs.sumneko-lua-language-server
-    pkgs.nodePackages.yaml-language-server
-    pkgs.nodePackages.typescript-language-server
-    pkgs.nodePackages.typescript
-    pkgs.nodePackages.pyright
-    pkgs.nodePackages.prettier
-    pkgs.rnix-lsp
-
-    # Rust Cli tools
-    pkgs.ripgrep
-    pkgs.tokei
-    pkgs.delta
-    pkgs.bat
-    pkgs.exa
-    pkgs.zoxide
-    pkgs.fd
-    pkgs.bottom
-    pkgs.diskonaut
-    pkgs.du-dust
-    pkgs.rust-analyzer
-
-    # Code managers
-    pkgs.gh
-    pkgs.glab
-
-    # Utils
-    pkgs.bash
-    pkgs.stow
-    pkgs.bitwarden-cli
-    pkgs.tmux
-    pkgs.jq
-    pkgs.fzf
-    pkgs.glow
-    pkgs.ncurses6
-    pkgs.kube3d
-  ];
 }
