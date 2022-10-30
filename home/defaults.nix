73de03591e56cc9700e0b3b5a36ffb4ff405abae
Cedric M'Passi <cempassi@student.42.fr>
2022-10-30 20:52:39 +0100

    nix: manage packages in default.nix

--- home/defaults.nix
+++ home/defaults.nix
@@ -1,7 +1,6 @@
-{
-  config,
-  pkgs,
-  ...
+{ config
+, pkgs
+, ...
 }: {
   imports = [
     ./alacritty/alacritty.nix
@@ -12,7 +11,19 @@
     ./packages.nix
   ];
 
+  fonts.fontconfig.enable = true;
+
   home = {
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "22.05";
+
     sessionPath = [
       "$HOME/.luarocks/bin" # Lua
       "$PYENV_ROOT/bin" #Pyenv
@@ -35,14 +46,43 @@
       NIX_PATH = "$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin";
     };
 
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    stateVersion = "22.05";
+    packages = [
+      # Rust Cli tools
+      pkgs.ripgrep
+      pkgs.tokei
+      pkgs.delta
+      pkgs.bat
+      pkgs.exa
+      pkgs.zoxide
+      pkgs.fd
+      pkgs.bottom
+      pkgs.diskonaut
+      pkgs.du-dust
+      pkgs.rust-analyzer
+
+      # Rust dev setup
+      pkgs.rust-bin.stable.latest.default
+
+      # Code managers
+      pkgs.gh
+      pkgs.glab
+
+      # Utils
+      pkgs.bash
+      pkgs.bitwarden-cli
+      pkgs.tmux
+      pkgs.jq
+      pkgs.fzf
+      pkgs.ncurses6
+      pkgs.kube3d
+
+      # font
+      (pkgs.nerdfonts.override { fonts = [ "Hasklig" ]; })
+    ];
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
   };
 }
