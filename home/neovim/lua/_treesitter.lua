0990b28c11d730ece7034f1383422165a85afd01
Cedric M'Passi <cempassi@student.42.fr>
2022-11-17 19:26:03 +0100

    neovim: add treesitter parsers

--- home/neovim/lua/_treesitter.lua
+++ home/neovim/lua/_treesitter.lua
@@ -1,7 +1,35 @@
 -- Tree-sitter configuration
+--
+local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
+vim.fn.mkdir(parser_install_dir, "p")
+vim.opt.runtimepath:append(parser_install_dir)
 
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { maintained }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = {
+    "c",
+		"rust",
+    "python",
+		"hcl",
+    "javascript",
+    "typescript",
+    "lua",
+    "bash",
+    "git_rebase",
+    "markdown",
+    "toml",
+    "json",
+    "dockerfile",
+    "make",
+    "svelte",
+    "scss",
+    "sql",
+    "http",
+    "css",
+    "yaml",
+    "vue",
+    "vim",
+	}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	parser_install_dir = parser_install_dir,
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		use_languagetree = false, -- Use this to enable language injection (this is very unstable)
@@ -17,5 +45,6 @@ require("nvim-treesitter.configs").setup({
 		persist_queries = false, -- Whether the query persists across vim sessions
 	},
 })
-local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
+
+local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
 ft_to_parser.telekasten = "markdown"
