0e6df00912f4b4a1bf523bcdbd3f1b9a78d69951
Cedric M'Passi <cempassi@student.42.fr>
2023-10-06 09:31:04 +0900

    nix: format flake.nix

--- flake.nix
+++ flake.nix
@@ -18,89 +18,88 @@
     homeage.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    inputs @ { self
-    , nixpkgs
-    , darwin
-    , home-manager
-    , alacritty-theme
-    , rust-overlay
-    , agenix
-    , homeage
-    , ...
-    }: {
-      homeConfigurations."cedric.mpassi@C02Z762ELVCF" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-darwin;
-        modules = [
-          homeage.homeManagerModules.homeage
-          {
-            homeage = {
-              # Absolute path to identity (created not through home-manager)
-              identityPaths = [ "~/.ssh/id_ed25519" ];
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    darwin,
+    home-manager,
+    alacritty-theme,
+    rust-overlay,
+    agenix,
+    homeage,
+    ...
+  }: {
+    homeConfigurations."cedric.mpassi@C02Z762ELVCF" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+      modules = [
+        homeage.homeManagerModules.homeage
+        {
+          homeage = {
+            # Absolute path to identity (created not through home-manager)
+            identityPaths = ["~/.ssh/id_ed25519"];
 
-              # "activation" if system doesn't support systemd
-              installationType = "activation";
+            # "activation" if system doesn't support systemd
+            installationType = "activation";
 
-              file."gitconfig-work" = {
-                # Path to encrypted file tracked by the git repository
-                source = ./.secrets/gitconfig-work.age;
-                # Path expected by git config: (./home/git/git.nix:12)
-                path = "/Users/cedric.mpassi/.config/git/work";
-              };
+            file."gitconfig-work" = {
+              # Path to encrypted file tracked by the git repository
+              source = ./.secrets/gitconfig-work.age;
+              # Path expected by git config: (./home/git/git.nix:12)
+              path = "/Users/cedric.mpassi/.config/git/work";
             };
-          }
-          ./home/macos.nix
-          ({ pkgs, ... }: {
-            nixpkgs.overlays = [
-              rust-overlay.overlays.default
-              alacritty-theme.overlays.default
-            ];
-          })
-        ];
-      };
-
-      darwinConfigurations.Balamb = darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
-        modules = [
-          ./system/darwin.nix
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.cempassi = import ./home/macos.nix;
-
-          }
-          ({ pkgs, ... }: {
-            nixpkgs.overlays = [
-              rust-overlay.overlays.default
-              alacritty-theme.overlays.default
-            ];
-          })
-        ];
-      };
-
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./system/nixos.nix
-          agenix.nixosModule
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.cempassi = import ./home/nixos.nix;
-          }
-          ({ pkgs, ... }: {
-            nixpkgs.overlays = [
-              rust-overlay.overlays.default
-              alacritty-theme.overlays.default
-            ];
-          })
-        ];
-      };
-      macosX86 = self.homeConfigurations."cedric.mpassi@C02Z762ELVCF".activationPackage;
-      defaultPackage.x86_64-darwin = self.macosX86;
-      defaultPackage.aarch64-darwin = self.darwinConfigurations.Balamb.system;
-      defaultPackage.x86_64-linux = self.nixosConfigurations.nixos;
+          };
+        }
+        ./home/macos.nix
+        ({pkgs, ...}: {
+          nixpkgs.overlays = [
+            rust-overlay.overlays.default
+            alacritty-theme.overlays.default
+          ];
+        })
+      ];
     };
+
+    darwinConfigurations.Balamb = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        ./system/darwin.nix
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.cempassi = import ./home/macos.nix;
+        }
+        ({pkgs, ...}: {
+          nixpkgs.overlays = [
+            rust-overlay.overlays.default
+            alacritty-theme.overlays.default
+          ];
+        })
+      ];
+    };
+
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./system/nixos.nix
+        agenix.nixosModule
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.cempassi = import ./home/nixos.nix;
+        }
+        ({pkgs, ...}: {
+          nixpkgs.overlays = [
+            rust-overlay.overlays.default
+            alacritty-theme.overlays.default
+          ];
+        })
+      ];
+    };
+    macosX86 = self.homeConfigurations."cedric.mpassi@C02Z762ELVCF".activationPackage;
+    defaultPackage.x86_64-darwin = self.macosX86;
+    defaultPackage.aarch64-darwin = self.darwinConfigurations.Balamb.system;
+    defaultPackage.x86_64-linux = self.nixosConfigurations.nixos;
+  };
 }
