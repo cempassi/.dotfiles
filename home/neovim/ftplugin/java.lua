2c0a42d32bdf64b01d4d0cba78062913d90a20bf
Cedric M'Passi <cempassi@student.42.fr>
2023-10-09 07:22:35 +0900

    nvim: fix jdtls config (working, not building)

--- home/neovim/ftplugin/java.lua
+++ home/neovim/ftplugin/java.lua
@@ -1,43 +1,71 @@
-local workspace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-print(vim.fn.glob(jdtls_path .. '/share/java/plugins/' .. 'org.eclipse.equinox.launcher_*.jar'))
+local workspace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
+local custom_on_attach = function(client, bufnr)
+	--- Load mappings defined by which-key per buffer
+	require("_which-key").lsp()
+
+	if client.config.flags then
+		client.config.flags.allow_incremental_sync = true
+	end
+
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+end
 
 local config = {
+	on_attach = custom_on_attach,
+	settings = {
+		java = {
+			errors = {
+				incompleteClasspath = {
+					severity = "ignore",
+				},
+			},
+		},
+	},
 	cmd = {
+		-- 💀
+		"jdt-language-server", -- or '/path/to/java17_or_newer/bin/java'
+		-- depends on if `java` is in your $PATH env variable and if it points to the right version.
 
-    -- 💀
-    'java', -- or '/path/to/java17_or_newer/bin/java'
-            -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Dosgi.checkConfiguration=true",
+		"-Dosgi.sharedConfiguration.area=#{etc}/jdtls",
+		"-Dosgi.sharedConfiguration.area.readOnly=true",
+		"-Dosgi.configuration.cascaded=true",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ALL",
+		"-Xmx1g",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens",
+		"java.base/java.util=ALL-UNNAMED",
+		"--add-opens",
+		"java.base/java.lang=ALL-UNNAMED",
 
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xmx1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+		-- 💀
+		"-jar",
+		vim.fn.glob(jdtls_path .. "/share/java/plugins/" .. "org.eclipse.equinox.launcher_*.jar"),
+		-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+		-- Must point to the                                                     Change this to
+		-- eclipse.jdt.ls installation                                           the actual version
 
-    -- 💀
-    '-jar', vim.fn.glob(jdtls_path .. '/share/java/plugins/' .. 'org.eclipse.equinox.launcher_*.jar'),
-         -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
-         -- Must point to the                                                     Change this to
-         -- eclipse.jdt.ls installation                                           the actual version
+		-- 💀
+		"-configuration",
+		vim.fn.expand("~/.cache/jdtls-config/"),
+		-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+		-- Must point to the                      Change to one of `linux`, `win` or `mac`
+		-- eclipse.jdt.ls installation            Depending on your system.
 
-
-    -- 💀
-    '-configuration', jdtls_path .. '/share/config',
-                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
-                    -- Must point to the                      Change to one of `linux`, `win` or `mac`
-                    -- eclipse.jdt.ls installation            Depending on your system.
-
-
-    -- 💀
-    -- See `data directory configuration` section in the README
-    '-data', vim.fn.expand('~/.cache/jdtls-workspace/') .. workspace_dir,
-  },
-	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+		-- 💀
+		-- See `data directory configuration` section in the README
+		"-data",
+		vim.fn.expand("~/.cache/jdtls-workspace/") .. workspace_dir,
+	},
+	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw", "pom.xml" }, { upward = true })[1]),
+	capabilities = capabilities,
 }
 
 require("jdtls").start_or_attach(config)
--- home/neovim/lua/modules/lsp/init.lua
+++ home/neovim/lua/modules/lsp/init.lua
@@ -6,14 +6,14 @@ require("modules.lsp._diagnostic")
 require("lspconfig.ui.windows").default_options.border = "single"
 
 local custom_on_attach = function(client, bufnr)
-  --- Load mappings defined by which-key per buffer
-  require("_which-key").lsp()
+	--- Load mappings defined by which-key per buffer
+	require("_which-key").lsp()
 
-  if client.config.flags then
-    client.config.flags.allow_incremental_sync = true
-  end
+	if client.config.flags then
+		client.config.flags.allow_incremental_sync = true
+	end
 
-  api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -21,182 +21,160 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- Servers Configuration --
 lspconfig.yamlls.setup({
-  on_attach = custom_on_attach,
-  capabilities = capabilities,
-  root_dir = vim.loop.cwd,
+	on_attach = custom_on_attach,
+	capabilities = capabilities,
+	root_dir = vim.loop.cwd,
 
-  settings = {
-    yaml = {
-      hover = true,
-      completion = true,
-      validate = true,
-      schemaStore = {
-        enable = true,
-      },
-      schemas = {
-        'https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json: "*.gitlab-ci.yml"',
-      },
-      customTags = { "!reference", "!Ref" },
-    },
-  },
+	settings = {
+		yaml = {
+			hover = true,
+			completion = true,
+			validate = true,
+			schemaStore = {
+				enable = true,
+			},
+			schemas = {
+				'https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json: "*.gitlab-ci.yml"',
+			},
+			customTags = { "!reference", "!Ref" },
+		},
+	},
 })
 
 lspconfig.vimls.setup({})
 
 lspconfig.taplo.setup({
-  on_attach = custom_on_attach,
+	on_attach = custom_on_attach,
 })
 
 lspconfig.tsserver.setup({
-  capabilities = capabilities,
-  filetypes = { "javascript", "typescript", "typescriptreact" },
-  on_attach = custom_on_attach,
-  cmd = { "typescript-language-server", "--stdio" },
-  autostart = true,
-  init_options = {
-    tsserver = {
-      path = tsserver_path,
-    },
-  },
-})
-
-local workspace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-
-lspconfig.jdtls.setup({
-  cmd = {
-    "jdt-language-server",
-    "-configuration",
-    jdtls_path .. "/share/config",
-    "-data",
-    vim.fn.expand("~/.cache/jdtls-workspace/") .. workspace_dir,
-  },
-  on_attach = custom_on_attach,
-  settings = {
-    java = {
-      errors = {
-        incompleteClasspath = {
-          severity = "ignore",
-        },
-      },
-    },
-  },
+	capabilities = capabilities,
+	filetypes = { "javascript", "typescript", "typescriptreact" },
+	on_attach = custom_on_attach,
+	cmd = { "typescript-language-server", "--stdio" },
+	autostart = true,
+	init_options = {
+		tsserver = {
+			path = tsserver_path,
+		},
+	},
 })
 
 lspconfig.html.setup({
-  on_attach = custom_on_attach,
-  capabilities = capabilities,
+	on_attach = custom_on_attach,
+	capabilities = capabilities,
 })
 
 lspconfig.cssls.setup({
-  on_attach = custom_on_attach,
-  capabilities = capabilities,
+	on_attach = custom_on_attach,
+	capabilities = capabilities,
 })
 
 lspconfig.rust_analyzer.setup({
-  on_attach = custom_on_attach,
-  capabilities = capabilities,
+	on_attach = custom_on_attach,
+	capabilities = capabilities,
 
-  settings = {
-    ["rust-analyzer"] = {
-      assist = {
-        importMergeBehaviour = "full",
-      },
+	settings = {
+		["rust-analyzer"] = {
+			assist = {
+				importMergeBehaviour = "full",
+			},
 
-      callInfo = {
-        full = true,
-      },
+			callInfo = {
+				full = true,
+			},
 
-      cargo = {
-        loadOutDirsFromCheck = true,
-      },
+			cargo = {
+				loadOutDirsFromCheck = true,
+			},
 
-      checkOnSave = {
-        allFeatures = true,
-        overrideCommand = {
-          "cargo",
-          "clippy",
-          "--workspace",
-          "--message-format=json",
-          "--all-targets",
-          "--all-features",
-        },
-      },
+			checkOnSave = {
+				allFeatures = true,
+				overrideCommand = {
+					"cargo",
+					"clippy",
+					"--workspace",
+					"--message-format=json",
+					"--all-targets",
+					"--all-features",
+				},
+			},
 
-      procMacro = {
-        enable = true,
-      },
-    },
-  },
+			procMacro = {
+				enable = true,
+			},
+		},
+	},
 })
 
 lspconfig.terraformls.setup({
-  on_attach = custom_on_attach,
-  capabilities = capabilities,
+	on_attach = custom_on_attach,
+	capabilities = capabilities,
 })
 
 lspconfig.vuels.setup({
-  on_attach = custom_on_attach,
-  capabilities = capabilities,
+	on_attach = custom_on_attach,
+	capabilities = capabilities,
 })
 
 lspconfig.clangd.setup({
-  on_attach = custom_on_attach,
-  capabilities = capabilities,
-  cmd = {
-    "clangd",
-    "--background-index",
-    "--suggest-missing-includes",
-    "--clang-tidy",
-    --"--header-insertion=iwyu",
-  },
-  init_options = {
-    clangdFileStatus = true,
-  },
+	on_attach = custom_on_attach,
+	capabilities = capabilities,
+	cmd = {
+		"clangd",
+		"--background-index",
+		"--suggest-missing-includes",
+		"--clang-tidy",
+		--"--header-insertion=iwyu",
+	},
+	init_options = {
+		clangdFileStatus = true,
+	},
 })
 
 lspconfig.lua_ls.setup({
-  cmd = { "lua-language-server" },
-  on_attach = custom_on_attach,
-  prefix = lua,
-  settings = {
-    Lua = {
-      runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
-      diagnostics = {
-        enable = true,
-        globals = {
-          "vim",
-          "describe",
-          "it",
-          "before_each",
-          "after_each",
-          "awesome",
-          "theme",
-          "client",
-          "P",
-          "tsserver_path",
-          "jdtls_path",
-        },
-        telemetry = {
-          enable = false,
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = {
-            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-            [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-          },
-        },
-      },
-    },
-  },
+	cmd = { "lua-language-server" },
+	on_attach = custom_on_attach,
+	prefix = lua,
+	settings = {
+		Lua = {
+			runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
+			diagnostics = {
+				enable = true,
+				globals = {
+					"vim",
+					"describe",
+					"it",
+					"before_each",
+					"after_each",
+					"awesome",
+					"theme",
+					"client",
+					"P",
+					"tsserver_path",
+					"jdtls_path",
+				},
+				telemetry = {
+					enable = false,
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+					},
+				},
+			},
+		},
+	},
 })
 
 lspconfig.pyright.setup({
-  on_attach = custom_on_attach,
-  capabilities = capabilities,
+	on_attach = custom_on_attach,
+	capabilities = capabilities,
 })
 
 lspconfig.rnix.setup({
-  on_attach = custom_on_attach,
-  capabilities = capabilities,
+	on_attach = custom_on_attach,
+	capabilities = capabilities,
 })
