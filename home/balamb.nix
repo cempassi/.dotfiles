b89c6c884a78c22ba482aeebcd1c35fd5f935233
Cedric M'Passi <cempassi@student.42.fr>
2023-10-19 06:35:25 +0200

    nix: better split between work and home configs

--- home/balamb.nix
+++ home/balamb.nix
@@ -11,9 +11,19 @@
     sessionVariables = {
       NIX_PATH = "$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin";
     };
+
+    packages = [
+      # Code managers
+      pkgs.gh
+
+      # Rust
+      (pkgs.rust-bin.stable.latest.default.override {
+        extensions = ["rust-analyzer" "rust-src"];
+      })
+    ];
   };
 
   programs.zsh.shellAliases = {
-    enix = "vim ~/.dotfiles/home/balamb.nix";
+    enix = "vim ~/.dotfiles/home/macos.nix";
   };
 }
--- home/work.nix
+++ home/work.nix
@@ -16,10 +16,30 @@
       NIX_PATH = "$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin";
       MAVEN_OPTS = "-Djavax.net.ssl.trustStore=/Users/cedric.mpassi/.local/certs/nexus.crt -Djavax.net.ssl.trustStorePassword=changeit";
     };
+
+    packages = [
+      # Code manager
+      pkgs.glab
+
+      # Python
+      (pkgs.python3.withPackages (ps:
+        with ps; [
+          pkgs.python310Packages.boto3
+          pkgs.python310Packages.requests
+          pkgs.python310Packages.beautifulsoup4
+          pkgs.python310Packages.truststore
+        ]))
+
+      # Utils
+      pkgs.awscli2
+
+    ]
+    ;
   };
 
   programs.zsh.shellAliases = {
     switch = "home-manager switch";
     enix = "vim ~/.dotfiles/home/macos.nix";
+    hack = "sudo dscl . -append /groups/admin GroupMembership cedric.mpassi";
   };
 }
