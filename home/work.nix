e5ecaa14b8b53db6cc5b90ccb42b80e4a5f39881
Cedric M'Passi <cempassi@student.42.fr>
2024-08-21 16:31:43 +0200

    nix: format code and add azure-cli and gnupg

--- home/work.nix
+++ home/work.nix
@@ -12,31 +12,31 @@
     username = "cedric.mpassi";
     homeDirectory = "/Users/cedric.mpassi";
     sessionVariables = {
-      ZSH_DISABLE_COMPFIX = "true" ;
+      ZSH_DISABLE_COMPFIX = "true";
       NODE_EXTRA_CA_CERTS = "${config.home.homeDirectory}/.local/certs/ca-bundle.crt";
-      NIX_CONFIG="experimental-features = nix-command flakes";
+      NIX_CONFIG = "experimental-features = nix-command flakes";
       NIX_PATH = "$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin";
       MAVEN_OPTS = "-Djavax.net.ssl.trustStore=/Users/cedric.mpassi/.local/certs/nexus.crt -Djavax.net.ssl.trustStorePassword=changeit";
     };
 
     packages = [
-
       # Jira
       pkgs.jira-cli-go
 
       # Python
-      (pkgs.python3.withPackages (ps:
-        with ps; [
-          pkgs.python311Packages.boto3
-          pkgs.python311Packages.botocore
-          pkgs.python311Packages.requests
-          pkgs.python311Packages.beautifulsoup4
-          pkgs.python311Packages.truststore
-        ]))
+      (pkgs.python3.withPackages (ps: [
+        pkgs.python311Packages.boto3
+        pkgs.python311Packages.botocore
+        pkgs.python311Packages.requests
+        pkgs.python311Packages.beautifulsoup4
+        pkgs.python311Packages.truststore
+      ]))
 
       # Utils
-      pkgs.azure-cli
-      pkgs.ssm-session-manager-plugin
+      (pkgs.azure-cli.withExtensions [pkgs.azure-cli.extensions.azure-devops pkgs.azure-cli.extensions.ssh])
+
+      pkgs.gnupg
+      pkgs.openssl
     ];
   };
 
