49ed2a04b2ce4527582f52f40f7ccbd9477f3d0c
Cedric M'Passi <cempassi@student.42.fr>
2025-02-16 11:05:31 +0100

    nvim: change leap.nvim match color

--- home/neovim/lua/_leap.lua
+++ home/neovim/lua/_leap.lua
@@ -3,6 +3,6 @@ if not ok then
     return
 end
 
-require("leap").set_default_keymaps()
+leap.set_default_keymaps()
+vim.api.nvim_set_hl(0, "LeapLabelPrimary", { link = "Label" })
 vim.api.nvim_set_hl(0, 'LeapBackdrop', { fg = 'grey' })
-vim.api.nvim_set_hl(0, "LeapLabelPrimary", { link = "Underlined" })
