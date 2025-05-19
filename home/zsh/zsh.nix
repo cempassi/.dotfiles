85c7f5929d2ee3c0e13a91c78e8c98c69f1c6e9a
Cedric M'Passi <cempassi@student.42.fr>
2025-05-19 21:57:01 +0200

    nix: correct zsh.nix warning

--- home/zsh/zsh.nix
+++ home/zsh/zsh.nix
@@ -57,7 +57,7 @@
       ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE = "fg=230";
     };
 
-    initExtra = ''
+    initContent = ''
       export NVM_DIR="$HOME/.nvm"
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
       [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
