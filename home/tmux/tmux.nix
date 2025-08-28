7b9649c40e7352129292caad244ce9521f8023de
Cedric M'Passi <cempassi@student.42.fr>
2025-08-28 21:45:29 +0200

    nix: update tmux config

--- home/tmux/tmux.nix
+++ home/tmux/tmux.nix
@@ -5,6 +5,11 @@
   programs.tmux = {
     enable = true;
     shell = "${pkgs.zsh}/bin/zsh";
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.resurrect
+      tmuxPlugins.continuum
+    ];
 
     extraConfig = builtins.readFile ./tmux.conf;
   };
